9d60b67446ff555b355a79e36e429ace69a67c18
Pakhi <9pakhisingh3@gmail.com>
2026-02-13 08:09:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            display.value = "";
+        } 
+        else if (value === "⌫") {
+            display.value = display.value.slice(0, -1);
+        } 
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        } 
+        else {
+            display.value += value;
+        }
+    });
+});
